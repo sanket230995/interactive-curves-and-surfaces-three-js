1dda17920290f926effee9303fb50f65e8ff0de3
Daniel P Pflager <daniel.pflager@gmail.com>
2021-06-09 17:58:26 -0700

    Temporarily comment-out animation code.

--- solutions/exercise-2.1/src/index.js
+++ solutions/exercise-2.1/src/index.js
@@ -30,10 +30,10 @@ function main() {
     camera.position.z = 5;
 
     const animate = function () {
-        requestAnimationFrame( animate );
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        // requestAnimationFrame( animate );
+        //
+        // cube.rotation.x += 0.01;
+        // cube.rotation.y += 0.01;
 
         renderer.render( scene, camera );
     };
